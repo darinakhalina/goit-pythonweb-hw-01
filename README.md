a742030951e82ad3a30364e4b7690bf6e6787c2b
Darina Khalina <darina.khalina@namecheap.com>
2025-01-13 15:04:42 +0200

    feat: add task 1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # goit-pythonweb-hw-01
+
+## Task 1
+### Завдання 1. Патерн фабрика
+
+![alt текст](./assets/hw1_1.png)
--- task_1.py
+++ task_1.py
@@ -1,23 +1,71 @@
-class Car:
-    def __init__(self, make, model):
-        self.make = make
-        self.model = model
-
-    def start_engine(self):
-        print(f"{self.make} {self.model}: Двигун запущено")
+from abc import ABC, abstractmethod
 
 
-class Motorcycle:
-    def __init__(self, make, model):
-        self.make = make
-        self.model = model
+class Vehicle(ABC):
+    def __init__(self, make: str, model: str, region: str) -> None:
+        self.make: str = make
+        self.model: str = model
+        self.region: str = region
 
-    def start_engine(self):
-        print(f"{self.make} {self.model}: Мотор заведено")
+    @abstractmethod
+    def start_engine(self) -> None:
+        pass
 
 
-vehicle1 = Car("Toyota", "Corolla")
-vehicle1.start_engine()
+class Car(Vehicle):
+    def start_engine(self) -> None:
+        print(f"{self.make} {self.model} {self.region}: Двигун запущено")
 
-vehicle2 = Motorcycle("Harley-Davidson", "Sportster")
-vehicle2.start_engine()
+
+class Motorcycle(Vehicle):
+    def start_engine(self) -> None:
+        print(f"{self.make} {self.model} {self.region}: Мотор заведено")
+
+
+class VehicleFactory(ABC):
+    @abstractmethod
+    def create_car(self, make: str, model: str) -> Vehicle:
+        pass
+
+    @abstractmethod
+    def create_motorcycle(self, make: str, model: str) -> Vehicle:
+        pass
+
+
+class USVehicleFactory(VehicleFactory):
+    def __init__(self) -> None:
+        self.region: str = "US Spec"
+
+    def create_car(self, make: str, model: str) -> Car:
+        return Car(make, model, self.region)
+
+    def create_motorcycle(self, make: str, model: str) -> Motorcycle:
+        return Motorcycle(make, model, self.region)
+
+
+class EUVehicleFactory(VehicleFactory):
+    def __init__(self) -> None:
+        self.region: str = "EU Spec"
+
+    def create_car(self, make: str, model: str) -> Car:
+        return Car(make, model, self.region)
+
+    def create_motorcycle(self, make: str, model: str) -> Motorcycle:
+        return Motorcycle(make, model, self.region)
+
+
+if __name__ == "__main__":
+    us_vehicle_factory = USVehicleFactory()
+    eu_vehicle_factory = EUVehicleFactory()
+
+    vehicle1 = us_vehicle_factory.create_car("Ford", "Mustang")
+    vehicle1.start_engine()
+
+    vehicle2 = eu_vehicle_factory.create_car("Audi", "A4")
+    vehicle2.start_engine()
+
+    vehicle3 = us_vehicle_factory.create_motorcycle("Harley-Davidson", "Street Glide")
+    vehicle3.start_engine()
+
+    vehicle4 = eu_vehicle_factory.create_motorcycle("BMW", "R1250GS")
+    vehicle4.start_engine()
